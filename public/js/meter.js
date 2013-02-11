378746f5dc496b55afbaa60a406c32cc1b3f7af7
Casey O'Hara <casey.ohara@imulus.com>
2013-02-10 22:05:33 -0700

    Sanity check the bounds of the meter value for color

--- public/js/meter.js
+++ public/js/meter.js
@@ -32,6 +32,9 @@ Meter.prototype.colorize = function() {
 }
 
 function colorFor(value) {
+  if (value < 0) value = 0;
+  if (value > 100) value = 100;
+
   this.colors = this.colors || (function() {
     return Range(0, 100).map(function(i) {
       var h = Math.floor(((i * 0.22) / 100) * 360),
